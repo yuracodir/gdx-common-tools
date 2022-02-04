fe065b137a1992d086a38549878e572c0d7451ee
yuracodir <sportabs02@gmail.com>
2022-02-04 20:58:27 +0300

    improved texture packing from directory

--- src/com/yuracodir/common/tools/resources/RClassPacker.kt
+++ src/com/yuracodir/common/tools/resources/RClassPacker.kt
@@ -7,153 +7,158 @@ import java.io.File
 
 class RClassPacker(private val configuration: Configuration = Configuration()) {
 
-    class Configuration @JvmOverloads constructor(
-        val packageName: String? = null,
-        val sourceResourcesDir: String = "core/res",
-        val outputDir: String = "$sourceResourcesDir/../build/generated/resources",
-        val providers: List<ResourceProvider> = listOf(
-            ResourceLabelSkinIdProvider(),
-            ResourceSpriteIdProvider(),
-            ResourceFileIdProvider("atlas", mask = ".atlas"),
-            ResourceFileIdProvider("sounds", mask = ".ogg"),
-            ResourceFileIdProvider("particle", mask = ".part"),
-            ResourceFileIdProvider("font", mask = ".fnt"),
-            ResourceFileIdProvider("skin", mask = ".json"),
-            ResourceFileIdProvider("values", "locale", "^strings"),
-            ResourceIdProvider("color"),
-            ResourceIdProvider("string", true)
-        )
+  class Configuration @JvmOverloads constructor(
+    val packageName: String? = null,
+    val sourceResourcesDir: String = "core/res",
+    val outputDir: String = "$sourceResourcesDir/../build/generated/resources",
+    val providers: MutableList<ResourceProvider> = mutableListOf(
+      ResourceLabelSkinIdProvider(),
+      ResourceSpriteIdProvider("atlas"),
+      ResourceFileIdProvider("atlas", mask = ".atlas"),
+      ResourceFileIdProvider("sounds", mask = ".ogg"),
+      ResourceFileIdProvider("particle", mask = ".part"),
+      ResourceFileIdProvider("font", mask = ".fnt"),
+      ResourceFileIdProvider("skin", mask = ".json"),
+      ResourceFileIdProvider("values", "locale", "^strings"),
+      ResourceIdProvider("color"),
+      ResourceIdProvider("string", true)
     )
+  )
 
-    private val providers = configuration.providers
+  private val providers = configuration.providers
 
 
-    fun process() {
-        File(configuration.sourceResourcesDir).tree()
-            .map {
-                it.replace(
-                    configuration.sourceResourcesDir.trimEnd(File.pathSeparatorChar) + File.pathSeparator,
-                    ""
-                )
-            }
-            .map { File(it) }
-            .let { file ->
-                providers.map { provider ->
-                    provider.getType() to (file.filter { provider.forFile(it) }.let {
-                        val values = mutableMapOf<String, String>()
-                        it.forEach { file -> values += provider.get(file) }
-                        values
-                    })
-                }.toMap()
-            }.map {
-                generateResourceClass(it.key, it.value)
-            }.joinToString("\n")
-            .let {
-                val classContent = generateClass(configuration.packageName, it)
-                val file = File(configuration.outputDir, "resources.kt")
-                file.apply { parentFile.mkdirs() }
-                    .writeText(classContent)
-            }
-    }
-
-    private fun generateClass(packageName: String?, content: String): String {
-        var output = if (packageName.isNullOrEmpty()) "" else "package $packageName\n\n"
-        output += "class R { \n"
-        output += "$content\n"
-        output += "}"
-        return output
-    }
-
-    private fun generateResourceClass(classname: String, values: Map<String, String>): String {
-        var output = ""
-        output += "  class $classname {\n"
-        output += "    companion object {\n"
-        values.forEach {
-            output += "      const val ${it.key} = \"${it.value.trim()}\"\n"
+  fun process() {
+    val sourceDirectory = File(configuration.sourceResourcesDir)
+    sourceDirectory.tree()
+      .map {
+        it.replace(
+          configuration.sourceResourcesDir.trimEnd(File.pathSeparatorChar) + File.pathSeparator,
+          ""
+        )
+      }
+      .map { File(it) }
+      .let { file ->
+        providers.associate { provider ->
+          provider.getType() to (file.filter { provider.forFile(it) }.let {
+            val values = mutableMapOf<String, String>()
+            it.forEach { file -> values += provider.get(sourceDirectory, file) }
+            values
+          })
         }
-        output += "    }\n"
-        output += "  }\n"
-        return output
+      }.map {
+        generateResourceClass(it.key, it.value)
+      }.joinToString("\n")
+      .let {
+        val classContent = generateClass(configuration.packageName, it)
+        val file = File(configuration.outputDir, "resources.kt")
+        file.apply { parentFile.mkdirs() }
+          .writeText(classContent)
+      }
+  }
+
+  private fun generateClass(packageName: String?, content: String): String {
+    var output = if (packageName.isNullOrEmpty()) "" else "package $packageName\n\n"
+    output += "class R { \n"
+    output += "$content\n"
+    output += "}"
+    return output
+  }
+
+  private fun generateResourceClass(classname: String, values: Map<String, String>): String {
+    var output = ""
+    output += "  class $classname {\n"
+    output += "    companion object {\n"
+    values.forEach {
+      output += "      const val ${it.key} = \"${it.value.trim()}\"\n"
     }
+    output += "    }\n"
+    output += "  }\n"
+    return output
+  }
 }
 
 interface ResourceProvider {
-    fun forFile(file: File): Boolean
-    fun getType(): String
-    fun get(file: File): Map<String, String>
+  fun forFile(file: File): Boolean
+  fun getType(): String
+  fun get(source: File, file: File): Map<String, String>
 }
 
-class ResourceSpriteIdProvider : ResourceProvider {
-    override fun forFile(file: File) = file.extension == "atlas"
+class ResourceSpriteIdProvider(private val directory: String) : ResourceProvider {
+  override fun forFile(file: File) = file.extension == "atlas"
 
-    override fun getType() = "sprite"
+  override fun getType() = "sprite"
 
-    override fun get(file: File): Map<String, String> {
-        val readLines = file.readLines()
-        return readLines.dropLast(1).mapIndexedNotNull { i, text ->
-            if (!text.startsWith(" ") && readLines[i + 1].startsWith(" ")) {
-                text.escape() to text
-            } else {
-                null
-            }
-        }.toMap()
+  override fun get(source: File, file: File): Map<String, String> {
+    if (file.parentFile.name == directory) {
+      val readLines = file.readLines()
+      return readLines.dropLast(1).mapIndexedNotNull { i, text ->
+        if (!text.startsWith(" ") && readLines[i + 1].startsWith(" ")) {
+          text.escape() to text
+        } else {
+          null
+        }
+      }.toMap()
     }
+    return emptyMap()
+  }
 }
 
 class ResourceLabelSkinIdProvider : ResourceProvider {
-    override fun forFile(file: File) = file.extension == "json"
+  override fun forFile(file: File) = file.extension == "json"
 
-    override fun getType() = "style"
+  override fun getType() = "style"
 
-    @Suppress("UNCHECKED_CAST")
-    override fun get(file: File): Map<String, String> {
-        val jsonMap = Json().fromJson(ObjectMap::class.java, file.readText())
-        val map = ObjectMap(jsonMap as ObjectMap<String, JsonValue>)
+  @Suppress("UNCHECKED_CAST")
+  override fun get(source: File, file: File): Map<String, String> {
+    val jsonMap = Json().fromJson(ObjectMap::class.java, file.readText())
+    val map = ObjectMap(jsonMap as ObjectMap<String, JsonValue>)
 
-        return jsonMap.keys()
-            .filter { it.contains("style", true) }
-            .flatMap { map[it] }
-            .map { it.name }
-            .associateBy { it.escape() }
-    }
+    return jsonMap.keys()
+      .filter { it.contains("style", true) }
+      .flatMap { map[it] }
+      .map { it.name }
+      .associateBy { it.escape() }
+  }
 }
 
 class ResourceFileIdProvider(
-    private val directory: String,
-    private val type: String = directory,
-    private val mask: String = ".*"
+  private val directory: String,
+  private val type: String = directory,
+  private val mask: String = ".*"
 ) : ResourceProvider {
-    override fun forFile(file: File) = file.name.matches(".*$mask.*".toRegex())
+  override fun forFile(file: File) = file.name.matches(".*$mask.*".toRegex())
 
-    override fun getType() = type
+  override fun getType() = type
 
-    override fun get(file: File): Map<String, String> {
-        if (file.parentFile.name == directory) {
-            val key = file.nameWithoutExtension.escape()
-            val value = directory + "/" + file.name
-            return mapOf(key to value)
-        }
-        return emptyMap()
+  override fun get(source: File, file: File): Map<String, String> {
+    val dir = file.parentFile.absolutePath.removePrefix(source.absolutePath + "/")
+    if (dir.startsWith(directory)) {
+      val key = file.nameWithoutExtension.escape()
+      val value = dir + "/" + file.name
+      return mapOf(key to value)
     }
+    return emptyMap()
+  }
 }
 
 class ResourceIdProvider(
-    private val type: String,
-    private val keyToKey: Boolean = false
+  private val type: String,
+  private val keyToKey: Boolean = false
 ) : ResourceProvider {
-    override fun forFile(file: File) = file.extension == "txt"
+  override fun forFile(file: File) = file.extension == "txt"
 
-    override fun getType() = type
+  override fun getType() = type
 
-    override fun get(file: File): Map<String, String> {
-        val content = file.readText()
-        return "(${type}.*):(.*)".toRegex()
-            .findAll(content)
-            .mapNotNull { result ->
-                result.value.split(":").takeIf { it.size == 2 }?.let {
-                    val (key, value) = it
-                    key.replace("$type.", "").escape() to if (keyToKey) key else value
-                }
-            }.toMap()
-    }
+  override fun get(source: File, file: File): Map<String, String> {
+    val content = file.readText()
+    return "(${type}.*):(.*)".toRegex()
+      .findAll(content)
+      .mapNotNull { result ->
+        result.value.split(":").takeIf { it.size == 2 }?.let {
+          val (key, value) = it
+          key.replace("$type.", "").escape() to if (keyToKey) key else value
+        }
+      }.toMap()
+  }
 }
--- src/com/yuracodir/common/tools/resources/TexturePacker.kt
+++ src/com/yuracodir/common/tools/resources/TexturePacker.kt
@@ -2,36 +2,45 @@ package com.yuracodir.common.tools.resources
 
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.tools.texturepacker.TexturePacker
+import com.badlogic.gdx.tools.texturepacker.TexturePackerFileProcessor
 import java.io.File
 
 class TexturesPacker(private val configuration: Configuration = Configuration()) {
 
-    class Configuration @JvmOverloads constructor(
-        val rawPath: String = "core/raw",
-        val output: String = "core/res/atlas",
-        val w: Int = 4096,
-        val h: Int = 4096,
-    )
+  class Configuration @JvmOverloads constructor(
+    val sourcePath: String = "core/raw",
+    val output: String = "core/res/atlas",
+    val w: Int = 4096,
+    val h: Int = 4096,
+    val smooth: Boolean = true
+  )
 
-    fun process() {
-        TexturePacker.Settings().apply {
-            maxWidth = configuration.w
-            maxHeight = configuration.h
-            filterMag = Texture.TextureFilter.Linear
-            filterMin = Texture.TextureFilter.Linear
-        }.let { settings ->
-            File(configuration.rawPath)
-                .tree()
-                .filter { it.toLowerCase().endsWith(".png") }
-                .map { File(it).parentFile }
-                .toSet().forEach {
-                    TexturePacker.process(
-                        settings,
-                        it.absolutePath,
-                        configuration.output,
-                        it.nameWithoutExtension
-                    )
-                }
+  fun process() {
+    TexturePacker.Settings().apply {
+      maxWidth = configuration.w
+      maxHeight = configuration.h
+      val textureFilter = if (configuration.smooth) Texture.TextureFilter.Linear else Texture.TextureFilter.Nearest
+      filterMag = textureFilter
+      filterMin = textureFilter
+    }.let { settings ->
+      val output = File(configuration.output, "atlas")
+      val source = File(configuration.sourcePath)
+      source
+        .tree()
+        .filter { it.toLowerCase().endsWith(".png") }
+        .map { File(it).parentFile }
+        .toSet().forEach {
+          try {
+            val name = it.absolutePath.removePrefix(source.absolutePath)
+              .trim(File.separatorChar)
+              .replace(File.separatorChar, '_')
+            val processor = TexturePackerFileProcessor(settings, name, null)
+            processor.setRecursive(false)
+            processor.process(it, output)
+          } catch (t: Throwable) {
+            t.printStackTrace()
+          }
         }
     }
+  }
 }
